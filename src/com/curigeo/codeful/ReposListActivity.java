807ebb5f4f98cb613df3b59d52442b0121b44446
bundai223 <bundai223@gmail.com>
2014-07-04 16:49:15 +0900

    Intellijでプロジェクトのベースを作成

---
+++ src/com/curigeo/codeful/ReposListActivity.java
@@ -0,0 +1,15 @@
+package com.curigeo.codeful;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReposListActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
